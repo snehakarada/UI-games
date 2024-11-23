8fe3cbf3d59221e5f844f15ae2bc38cd5f7d6f20
bhagya lakshmi <146413665+snehakarada@users.noreply.github.com>
2024-11-23 13:52:27 +0530

    Update guessNumber.js

--- guessNumber.js
+++ guessNumber.js
@@ -1,6 +1,4 @@
-// change repeat() function name
-
-function repeat() {
+function repeatGame() {
   console.log('sorry you lost the game😔');
   const repeat = confirm("Do you want to play again😉");
 
@@ -12,21 +10,24 @@ function guessNumber(choices) {
   const randomNum = Math.ceil(Math.random() * 10);
 
   if (choices === 1) {
-    return repeat();
+    return repeatGame();
   }
 
   if (userGuess === randomNum) {
     return "HEY! you won the game...🏆";
   }
-  const diff  = Math.abs(userGuess - randomNum);
-  let feedback = 'your guess is '
-  feedback += diff < 4 ? 'close to secret guess' : 'far away from secret guess';
 
-  console.log(feedback + " this is the last chance try your best🥵");
+  const diff  = Math.abs(userGuess - randomNum);
+
+  let feedback = 'your guess is ';
+  feedback += diff < 3 ? 'close to secret guess' : 'far away from secret guess';
+
+  console.log('\n', feedback + " this is the last chance try your best🥵");
+
   return guessNumber(choices - 1)
 }
 
 const chances = 2;
 
-console.log("guess the number between 1 to 9 in TWO chances");
+console.log(" 💡guess the number between 1 to 9 in TWO chances 💡");
 console.log(guessNumber(chances));
